b976c428060610f3ceafd7dd70e2bfff6cca17cc
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-27 23:51:59 +0530

    commit

--- assets/styles/js/password.js
+++ assets/styles/js/password.js
@@ -1,27 +1,5 @@
 import $ from 'jquery';
-import alertify from 'alertifyjs';
-$(document).on('click', function (e) {
+$(document).on('click', '.togglePassword', function () {
 
-    e.preventDefault();
-    let productId = $(this).data('product-id');
-    let productPrice = $(this).data('product-price');
-    let productName = $(this).data('product-name');
+});
 
-    $.ajax({
-        type: "POST",
-        url: `/user/add-to-cart/${productId}`,
-        data: {
-            productId: productId,
-            productPrice: productPrice,
-            productName: productName,
-        },
-        success: function (response) {
-            if(!response.status){
-                alertify.error(response.msg);
-                if(response.redirect){
-                    window.location = response.redirect;
-                }
-            }
-        }
-    })
-})
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+
 class User extends AbstractEntity implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Column(length: 180)]
