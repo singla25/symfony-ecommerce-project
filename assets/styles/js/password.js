7c710fd6abbc6bffc4da0b04ef2ea42af3f454f8
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-07-28 11:57:55 +0530

    hide-show-password

--- assets/styles/js/password.js
+++ assets/styles/js/password.js
@@ -1,5 +1,17 @@
 import $ from 'jquery';
-$(document).on('click', '.togglePassword', function () {
+import alertify from 'alertifyjs';
+$(document).on('click', '[data-action="hide_show"]', function () {
+    let $eye= $(this);
+    const id = $eye.prev().attr('id');
 
+    const passwordField = $('#' + id);
+
+    const fieldType = passwordField.prop('type');
+
+    if (fieldType === 'password') {
+        passwordField.prop('type', 'text');
+    } else {
+        passwordField.prop('type', 'password');
+    }
 });
 
