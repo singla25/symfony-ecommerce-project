971367ac9a681469108c7e101c4cf3e720274be8
Sahil Singla <98223428+singla25@users.noreply.github.com>
2025-08-02 01:32:34 +0530

    form : choiceType

--- src/Form/Shop/ProductType.php
+++ src/Form/Shop/ProductType.php
@@ -4,6 +4,7 @@ namespace App\Form\Shop;
 
 use App\Entity\Shop\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,13 +17,35 @@ class ProductType extends AbstractType
             ->add('brandName')
             ->add('productName')
             ->add('productType')
-            ->add('productDetail', TextareaType::class, [
-                'required' => false,
-                'label' => false,
+            ->add('productDetail', TextareaType::class)
+            ->add('size', ChoiceType::class, [
+                'choices' => [
+                    'S' => 'S',
+                    'M' => 'M',
+                    'L' => 'L',
+                    'XL' => 'XL',
+                    'XXL' => 'XXL',
+                    'XXXL' => 'XXXL'
+                ],
+                'placeholder' => 'Select',
+                'multiple' => true,
+                'expanded' => false,
+                'attr' => [
+                    'class' => 'form-control select2 rounded-3 shadow-sm',
+                    'multiple' => 'multiple',
+                ]
             ])
             ->add('price')
             ->add('rating')
             ->add('photo')
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    'Male' => 'Male',
+                    'Female' => 'Female',
+                    'Child' => 'Child',
+                ],
+                'placeholder' => 'Select',
+            ])
         ;
     }
 
